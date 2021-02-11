db0d36a2e0acd383a7d066f1c37bfb48b84bb94e
GinoChasles <gino.chasles@gmail.com>
2021-02-11 15:46:42 +0100

    init projet spring

---
+++ src/test/java/fr/gino/exo_spring/ExoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gino.exo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
